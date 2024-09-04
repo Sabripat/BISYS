f961f6a1fdb29ad86d0162b19425622468291c6f
Sabripat <patinosab@gmail.com>
2024-09-04 14:51:56 -0300

    Añadido .gitignore para ignorar el entorno virtual y otros cambios

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory, render_template
+
+app = Flask(__name__, static_folder='../frontend', template_folder='../templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('../frontend', path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
